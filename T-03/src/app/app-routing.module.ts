317e2f2ceb43b0bbef1df3f67975a55570d8bd72
Bruno Rodrigues <38739621+rdbruno@users.noreply.github.com>
2021-10-03 23:41:57 -0300

    codigo-fonte

---
+++ T-03/src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from './shared/guards/auth-guard';
+import { MenuComponent } from './shared/components/menu/menu.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'acesso' },
+  {
+    path: 'acesso',
+    loadChildren: () => import('./modules/acesso/acesso.module').then(m => m.AcessoModule)
+  },
+  {
+    path: 'home',
+    component: MenuComponent,
+    canActivate: [ AuthGuard ],
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'salas'
+      },
+      { 
+        path: 'salas',
+        loadChildren: () => import('./modules/salas/salas.module').then(m => m.SalasModule)
+      },
+      {
+        path: 'conta',
+        loadChildren: () => import('./modules/conta/conta.module').then(m => m.ContaModule)
+      },
+      {
+        path: 'carrinho',
+        loadChildren: () => import('./modules/carrinho/carrinho.module').then(m => m.CarrinhoModule)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
