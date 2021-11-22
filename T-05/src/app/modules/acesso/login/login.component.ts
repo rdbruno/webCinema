3cf78537a1cf8ef2f4e15dd2208d7541f2931aaf
Bruno Rodrigues <38739621+rdbruno@users.noreply.github.com>
2021-11-21 22:55:44 -0300

    codigo-fonte

---
+++ T-05/src/app/modules/acesso/login/login.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
+
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { Usuario } from 'src/app/shared/models/usuario.model';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  public formularioLogin = this.formBuilder.group({
+    email: [null, [Validators.required, Validators.email]],
+    senha: [null, Validators.required],
+  });
+
+  constructor(
+    private router: Router, 
+    private formBuilder: FormBuilder, 
+    private authService: AuthService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  public signIn(): void {
+    const usuario: Usuario = new Usuario(
+      '',
+      '',
+      this.formularioLogin.value.email,
+      '',
+      this.formularioLogin.value.senha
+    );
+
+    this.authService.login(usuario)
+      .subscribe(res => {
+        localStorage.setItem('Login', 'True');
+        localStorage.setItem('UserID', res.Login[0].Id);
+        localStorage.setItem('UserDoc', res.Login[0].RgCpf);
+        localStorage.setItem('Fidelidade', res.Login[0].BitFidelidade);
+        this.router.navigate(['home']);
+      },
+      erro => {
+        console.log(erro);
+      })    
+  }
+
+}
